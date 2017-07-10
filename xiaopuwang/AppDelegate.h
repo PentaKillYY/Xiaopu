bdcff4443bfb1104737e9a1d1ec643856151e6cd
LuyangHuang <TonyJiang@TonyJiangdeMacBook-Pro.local>
2017-07-10 17:10:59 +0800

    添加工程

---
+++ xiaopuwang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaopuwang
+//
+//  Created by TonyJiang on 17/7/10.
+//  Copyright © 2017年 ings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiaopuwang/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  xiaopuwang
+//
+//  Created by TonyJiang on 17/7/10.
+//  Copyright © 2017年 ings. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
