59db30ada5b6909b88a2df32808381a21e9adbc4
LuyangHuang <TonyJiang@TonyJiangdeMacBook-Pro.local>
2017-07-13 14:01:24 +0800

    修改主页结构

--- xiaopuwang/MainViewController.m
+++ xiaopuwang/MainViewController.m
@@ -12,10 +12,12 @@
 #import "MainServiceTableViewCell.h"
 #import "MainActivityTableViewCell.h"
 #import "MainPreferedTableViewCell.h"
-
-@interface MainViewController ()<UITableViewDelegate,UITableViewDataSource>
-
-
+#import "UIButton+JKImagePosition.h"
+#import "UIButton+JKMiddleAligning.h"
+@interface MainViewController ()<UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate>
+@property (nonatomic,strong)IBOutlet UISearchBar* searchBar;
+@property (nonatomic,strong)IBOutlet UIButton* rightButton;
+@property (nonatomic,strong)IBOutlet UIButton* leftButton;
 @end
 
 @implementation MainViewController
@@ -23,9 +25,93 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self changeStatusBarStyle:UIStatusBarStyleLightContent statusBarHidden:NO changeStatusBarAnimated:YES];
-     [self hideNavigationBar:YES animated:YES];
+    
+    [self changeNavTitleView];
 }
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear: YES];
+   
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:YES];
+    
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    
+}
+
++ (UIImage*) imageWithColor:(UIColor*)color andHeight:(CGFloat)height
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, height);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+- (void)changeNavTitleView{
+    _leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _leftButton.titleLabel.font = [UIFont systemFontOfSize:10.0];
+    [_leftButton setTitle:@"无锡" forState:0];
+    [_leftButton setImage:V_IMAGE(@"map") forState:0];
+    [_leftButton jk_setImagePosition:2 spacing:0];
+
+//    UIView *leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 5, 44, <#CGFloat height#>)]
+    
+    NSString *content = _leftButton.titleLabel.text;
+    UIFont *font = _leftButton.titleLabel.font;
+    CGSize size = CGSizeMake(MAXFLOAT, 44.0f);
+    CGSize buttonSize = [content boundingRectWithSize:size
+                                              options:NSStringDrawingTruncatesLastVisibleLine  | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                           attributes:@{ NSFontAttributeName:font}
+                                              context:nil].size;
+    if (buttonSize.width < 44 ) {
+        [_leftButton setFrame:CGRectMake(0, 0, 44, 44)];
+    }else if (buttonSize.width  > 50 ){
+        [_leftButton setFrame:CGRectMake(0, 0, 50, 44)];
+    }else{
+        [_leftButton setFrame:CGRectMake(0, 0, buttonSize.width, 44)];
+    }
+
+    [_leftButton setContentEdgeInsets:UIEdgeInsetsMake(10, 0, 0, 0)];
+    _searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, Main_Screen_Width-88, 44)];
+    UIImage* clearImg = [MainViewController imageWithColor:[UIColor clearColor] andHeight:44.0f];
+    [_searchBar setBackgroundImage:clearImg];
+    _searchBar.placeholder = @"请输入机构、学校、课程名称 ";
+    [_searchBar setBackgroundColor:[UIColor clearColor]];
+    
+    _rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_rightButton setFrame:CGRectMake(0, 0, 44, 44)];
+    [_rightButton setImageEdgeInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [_rightButton setImage:V_IMAGE(@"email") forState:0];
+    
+    UIBarButtonItem *negativeSpacer = [[UIBarButtonItem alloc]
+                                       initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace
+                                       target:nil action:nil];
+    negativeSpacer.width = -10;
+
+    
+    UIBarButtonItem* leftItem = [[UIBarButtonItem alloc] initWithCustomView:_leftButton];
+    UIBarButtonItem* rightItem = [[UIBarButtonItem alloc] initWithCustomView:_rightButton];
+    
+    self.navigationItem.leftBarButtonItems = @[negativeSpacer,leftItem];
+    self.navigationItem.rightBarButtonItems = @[negativeSpacer,rightItem];
+    
+    self.navigationItem.titleView = _searchBar;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -52,7 +138,7 @@
         return cell;
     }else if (indexPath.section == 2){
         MainServiceTableViewCell* cell = [[NSBundle mainBundle] loadNibNamed:@"MainServiceTableViewCell" owner:self options:nil].firstObject;
-               return cell;
+        return cell;
     }else if (indexPath.section == 5){
         MainPreferedTableViewCell* cell = [[NSBundle mainBundle] loadNibNamed:@"MainPreferedTableViewCell" owner:self options:nil].firstObject;
         cell.atitleView.image = V_IMAGE(@"校谱优选");
@@ -102,8 +188,20 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+- (void)leftItemAction:(id)sender{
     
 }
 
+- (void)rightItemAction:(id)sender{
 
+}
+
+//当键盘出现
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    [_searchBar resignFirstResponder];
+    [self performSegueWithIdentifier:@"MainSearch" sender:self];
+}
 @end
---
+++ xiaopuwang/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  xiaopuwang
+//
+//  Created by TonyJiang on 2017/7/13.
+//  Copyright © 2017年 ings. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SearchViewController : BaseViewController
+
+@end
