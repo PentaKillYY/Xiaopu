86c7ba2ee178d9d65786777410401650457732b0
LuyangHuang <TonyJiang@TonyJiangdeMacBook-Pro.local>
2017-07-17 14:29:00 +0800

    新增自主择校页面

---
+++ xiaopuwang/PersonalChooseViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalChooseViewController.h
+//  xiaopuwang
+//
+//  Created by TonyJiang on 2017/7/17.
+//  Copyright © 2017年 ings. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PersonalChooseViewController : BaseViewController
+
+@end
