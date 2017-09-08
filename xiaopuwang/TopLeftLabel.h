4d38dfc21cc70eb858810ffa13539db20a9cfd1f
LuyangHuang <TonyJiang@TonyJiangdeMacBook-Pro.local>
2017-09-08 15:25:04 +0800

    修复bug

---
+++ xiaopuwang/TopLeftLabel.h
@@ -0,0 +1,13 @@
+//
+//  TopLeftLabel.h
+//  xiaopuwang
+//
+//  Created by TonyJiang on 2017/9/8.
+//  Copyright © 2017年 ings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopLeftLabel : UILabel
+
+@end
